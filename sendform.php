800c792ea9b53c374fa236157b4c230ddb8bfa89
Stepan213 <cimlerstepan@gmail.com>
2019-08-25 10:10:47 +0200

    Better error message handling, still TODO

--- sendform.php
+++ sendform.php
@@ -8,55 +8,57 @@ $temp = explode(".", $_FILES["fileToUpload"]["name"]);
 $URL = round(microtime(true));
 $newfilename = $URL . '.' . end($temp);
 
+$error_message = null;
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         $upload_ok = 1;
     } else {
-        echo "Zvolený soubor není obrázek.";
+        $error_message = "Zvolený soubor pravý není obrázek.";
         $upload_ok = 0;
     }
 }
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Obrázek se nezdařilo nahrát, interní chyba: Shoda systémových jmen. Zkuste to prosím znovu.";
+if (file_exists($target_file) && $upload_ok) {
+    $error_message = "Obrázek se nezdařilo nahrát, interní chyba: Shoda systémových jmen.";
     $upload_ok = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Nahraný obrázek je větší než 5MB.";
+if ($_FILES["fileToUpload"]["size"] > 5000000 && $upload_ok) {
+    $error_message = "Nahraný obrázek je větší než 5MB.";
     $upload_ok = 0;
 }
 // Allow certain file formats
 if($image_file_type != "jpg" && $image_file_type != "png" && $image_file_type != "jpeg"
-&& $image_file_type != "gif" ) {
-    echo "Omlouvám se, pouze JPG, JPEG, PNG a GIF formáty jsou povolené.";
+&& $image_file_type != "gif"  && $upload_ok) {
+    $error_message = "Pouze JPG, JPEG, PNG a GIF formáty jsou povolené.";
     $upload_ok = 0;
 }
-// Check if $upload_ok is set to 0 by an error
-if ($upload_ok == 0) {
-    echo "Omlouvám se, obrázek se nepodařilo nahrát. Zkuste to prosím znovu.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_img_dir . $newfilename)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Omlouvám se, obrázek se nepodařilo nahrát. Zkuste to prosím znovu.";
-    }
+
+// Try upload file
+if($upload_ok) {
+  if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_img_dir . $newfilename)) {
+    $error_message = "Omlouvám se, obrázek se nepodařilo nahrát. Zkuste to prosím znovu.";
+  }
 }
 
+// Determine if ad is single book or group (only single book form has 'subject' field)
+$group = !isset($_POST['subject']);
 
 // Get variables from html form
 $book_name = $_POST['bookname'];
-$subject = $_POST['subject'];
 $school_year = $_POST['schoolyear'];
-$price = $_POST['price'];
 $note = $_POST['note'];
 $user_name = $_POST['username'];
 $user_mail = $_POST['mail'];
 $other_contact = $_POST['othercontact'];
 $unhashed_password = $_POST['password'];
+if(!$group) {
+  $subject = $_POST['subject'];
+  $price = $_POST['price'];
+}
 
 // Get photo URL
 $photoURL = $target_img_dir . $newfilename;
@@ -75,19 +77,29 @@ if($conn->connect_error) {
   die('Connect Error:' . $conn->connect_error);
 }
 
-// Prepare statement, avoid injection attack
-$sql = $conn->prepare("INSERT INTO main (BookName, Subject, SchoolYear, URL, PhotoURL, Price, Note, UserName, Mail, OtherContact, Password) VALUES
-(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-// Bind values
-$sql->bind_param("ssississsss", $book_name, $subject, $school_year, $URL, $photoURL, $price, $note, $user_name, $user_mail, $other_contact, $password);
+if($group) {
+  // Prepare statement, avoid injection attack
+  $sql = $conn->prepare("INSERT INTO main (BookName, SchoolYear, URL, PhotoURL, Note, UserName, Mail, OtherContact, Password, IsGroup) VALUES
+  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  // Bind values
+  $sql->bind_param("sisssssssi", $book_name, $school_year, $URL, $photoURL, $note, $user_name, $user_mail, $other_contact, $password, $group);
+} else {
+  // Prepare statement, avoid injection attack
+  $sql = $conn->prepare("INSERT INTO main (BookName, Subject, SchoolYear, URL, PhotoURL, Price, Note, UserName, Mail, OtherContact, Password) VALUES
+  (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  // Bind values
+  $sql->bind_param("ssississsss", $book_name, $subject, $school_year, $URL, $photoURL, $price, $note, $user_name, $user_mail, $other_contact, $password);
+}
 
 // Execute, check if successful, redirect
-if($sql->execute()) {
+if($sql->execute() && !$error_message) {
   header("Location: ad.php?URL=" . $URL);
   die();
 } else {
-  "<p class='error-message'>Při přidávání se něco pokazilo :-/ Chceš to <a href='new.html'>zkusit znovu</a>?<p>";
+  echo "<p class='error-message'>Při přidávání se něco pokazilo :-/ Chceš to <a href='new.html'>zkusit znovu</a>?<p>";
+  if($error_message) {
+    echo "<p class='error-message'>Problémy udělal obrázek: " . $error_message . "<p>";
+  }
 }
 
 $sql->close();
