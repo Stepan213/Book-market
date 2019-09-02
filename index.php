fbe5352513e9f7e74fd1892e0f65b9e39bca42f9
Stepan213 <cimlerstepan@gmail.com>
2019-09-02 20:52:33 +0200

    Style changes.

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
     <link rel="stylesheet" href="css/master.min.css">
     <title>Online burza učebnic</title>
   </head>
-  <body>
+  <body id="main-page-wrapper">
     <header id="main-page-header">
       <?php include 'php-chunks/header.php' ?>
     </header>
     <div id="main-page">
       <h2>Ahoj, vítej na burze učebnic!</h2>
       <p>Chceme studentům pomoci prodat a nakoupit učebnice. K tomu slouží tato burza, kde je můžeš vystavit, nebo si prohlížet inzeráty ostatních.</p>
-      <p>Funguje to jednoduše: Přidáš inzerát, k tomu svůj kontakt, a když se tvé učebnice někomu zalíbí, dá ti vědět.</p>
-      <p>V současné době se zaměřujeme jenom na učebnice pro vyšší gympl v Roudnici nad Labem, ale to se může kdykoli změnit :-)</p>
       <p id="main-page-buttons-heading">Tak s čím mohu pomoci?</p>
       <div id="main-page-buttons">
         <a href="new-choice.php">Prodat</a>
         <a href="ads.php">Koupit</a>
       </div>
+      <p>Funguje to jednoduše: Přidáš inzerát, k tomu svůj kontakt, a když se tvé učebnice někomu zalíbí, dá ti vědět.</p>
+      <p>V současné době se zaměřujeme jenom na učebnice pro vyšší gympl v Roudnici nad Labem, ale to se může kdykoli změnit :-)</p>
     </div>
     <footer>
       <p>© Ucebnicovka.cz 2019</p>
